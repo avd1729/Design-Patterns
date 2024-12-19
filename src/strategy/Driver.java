d6cb845e31ce9702b86edb20fab84b76853094b1
Aravind <aravind19112003@gmail.com>
2024-12-19 12:26:33 +0530

    first commit

--- src/strategy/Driver.java
+++ src/strategy/Driver.java
@@ -2,8 +2,8 @@ package strategy;
 
 public class Driver {
     public static void main(String[] args) {
-        strategy.PaymentStrategy paymentStrategy = new strategy.CreditCardPayment();
-        strategy.ShoppingCart cart = new strategy.ShoppingCart();
+        PaymentStrategy paymentStrategy = new GooglePayPayment();
+        ShoppingCart cart = new ShoppingCart();
         cart.setPaymentStrategy(paymentStrategy);
         cart.checkout(500);
     }
--- src/strategy/ShoppingCart.java
+++ src/strategy/ShoppingCart.java
@@ -1,11 +1,10 @@
 package strategy;
 
 public class ShoppingCart {
+
     private PaymentStrategy paymentStrategy;
     public ShoppingCart() {
-
     }
-
     public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
         this.paymentStrategy = paymentStrategy;
     }
